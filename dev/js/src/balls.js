aa1dfec106a68b01ad4a270db90f795da8b824fb
Nick McMillan <hey.nick@live.com.au>
2015-11-14 00:12:47 +1100

    js linting

--- dev/js/src/balls.js
+++ dev/js/src/balls.js
@@ -4,14 +4,10 @@
 app.balls = (function () {
 	'use strict';
 
-	var containerEl = app.globals.doc.getElementById('container');
-
 	// OPTIONS
 	var ballRoughness = 0.88, // 1 === perfect circle
 		minBallSize = 30,
 		maxBallSize = 75,
-
-		spareCount = 5,
 		spreadPush = 1.05, // how hard the balls push against each other
 		spreadSpeed = 0.075;
 
@@ -50,17 +46,15 @@ app.balls = (function () {
 
 	//http://www.colourlovers.com/palette/396423/Praise_Certain_Frogs
 	var ballColors = [
-	  '#F4FCE8', // dark blue
-	  '#C3FF68', // green
-	  '#87D69B', // blue
-	  '#4E9689', // metal blue
-	  '#7ED0D6'
+		'#F4FCE8', // dark blue
+		'#C3FF68', // green
+		'#87D69B', // blue
+		'#4E9689', // metal blue
+		'#7ED0D6'
 	];
 
 	var innerWidthHalf = window.innerWidth / 2,
 		innerHeightHalf = window.innerHeight / 2;
-
-	var articleCount = dataModel.length;
 
 	var createCircleCounter = 0;
 
@@ -68,23 +62,16 @@ app.balls = (function () {
 
 		// need to check that datamodel contains certain properties before we can assign to them
 		var thisCircle = dataModel[createCircleCounter],
-			checkDataModel = type !== 'spare' ? true : false;
+			checkDataModel = type === 'article' ? true : false;
 
 
-		var href = checkDataModel ? thisCircle.href : null,
-			img = checkDataModel ? thisCircle.img : null,
-			text = checkDataModel ? thisCircle.text : null,
-			r = checkDataModel ? thisCircle.r : app.utilities.random(minBallSize, maxBallSize),
+		var r = checkDataModel ? thisCircle.r : app.utilities.random(minBallSize, maxBallSize),
 			fill = checkDataModel ? thisCircle.fill : ballColors[Math.floor(Math.random() * ballColors.length)];
 
 
 		var circleObj = {
-			isTitleBall: false,
-			href: href,
-			img: img,
-			fill: fill,
-			text: text,
 			id: createCircleCounter,
+			fill: fill,
 			x: innerWidthHalf + Math.random(),
 			y: innerHeightHalf + Math.random(),
 			vx: 0,
@@ -97,11 +84,20 @@ app.balls = (function () {
 			dragForceX: 0,
 			velocityX: 0,
 			velocityY: 0,
-
 			dragStartPositionX: 0,
 			dragStartPositionY: 0,
 			r: r
 		};
+
+		if (type === 'article') {
+
+			circleObj.href = thisCircle.href;
+			circleObj.img = thisCircle.img;
+			circleObj.text = thisCircle.text;
+
+			createCircleCounter++;
+
+		}
 
 
 		if (type === 'title') {
@@ -109,27 +105,16 @@ app.balls = (function () {
 			circleObj.fill = '#ddd';
 			circleObj.r = 170;
 		}
-		if (type === 'article') {
-			articleCount++;
-		}
-		if (type === 'spare') {
-			spareCount++
-		}
-
-		createCircleCounter++;
 
 		return circleObj;
 
-	}
+	};
 
 
 	var init = function () {
 
 
-
-
-		var m,
-			c,
+		var c,
 			dx,
 			dy,
 			d,
@@ -140,51 +125,49 @@ app.balls = (function () {
 			currentCircle;
 
 
-		// ensure that the circles don't exceed the limit
-		// could use work for better randomisation
-
-		for (var i = 0; i < 4; i++) {
-			app.globals.circleArr.push( createCircle('article') );
-
+		for (let i = 0; i < 10; i++) {
+			app.globals.circleArr.push( createCircle('spare') );
 		}
-		// for (var i = 0; i < 2; i++) {
-		// 	app.globals.circleArr.push( createCircle('article') );
-		//
-		// }
+
+		for (let i = 0; i < dataModel.length; i++) {
+			app.globals.circleArr.push( createCircle('article') );
+		}
+
+		app.globals.circleArr.push( createCircle('title') );
 
 
 		function renderLoop() {
 
-		    for (var i = 0; i < app.globals.circleArr.length; i++) {
+			for (let i = 0; i < app.globals.circleArr.length; i++) {
 
-		    	currentCircle = app.globals.circleArr[i];
+				currentCircle = app.globals.circleArr[i];
 
-		    	// circle packing, based off:
-		    	// http://codepen.io/jun-lu/pen/rajrJx
-		    	for (var j = 0; j < app.globals.circleArr.length; j++) {
+				// circle packing, based off:
+				// http://codepen.io/jun-lu/pen/rajrJx
+				for (let j = 0; j < app.globals.circleArr.length; j++) {
 
-		    		if (i === j) {
-		    			continue; // skip this loop
-		    		}
+					if (i === j) {
+						continue; // skip this loop
+					}
 
-			    	c = app.globals.circleArr[j];
-			    	d = (dx = c.x - currentCircle.x) * dx + (dy = c.y - currentCircle.y) * dy;
-			    	l = (r = currentCircle.r + c.r) * r * spreadPush;
+					c = app.globals.circleArr[j];
+					d = (dx = c.x - currentCircle.x) * dx + (dy = c.y - currentCircle.y) * dy;
+					l = (r = currentCircle.r + c.r) * r * spreadPush;
 
-			    	if (d < l) {
+					if (d < l) {
 
-			    		f = (1-d/l) * r;
-			    		t = Math.atan2(dy, dx);
+						f = (1 - d / l) * r;
+						t = Math.atan2(dy, dx);
 
-			    		// set right edge && left edge boundaries
-			    		var hozBoundary = c.x + 20 < window.innerWidth - c.r && c.x > 20 + c.r, // 20 get away from edges in case text goes over boundary
-			    			verBoundary = c.y + 60 < window.innerHeight - c.r && c.y > 0 + c.r; // +60 to keep some spacing at bottom for label
+						// set right edge && left edge boundaries
+						var hozBoundary = c.x + 20 < window.innerWidth - c.r && c.x > 20 + c.r, // 20 get away from edges in case text goes over boundary
+								verBoundary = c.y + 60 < window.innerHeight - c.r && c.y > 0 + c.r; // +60 to keep some spacing at bottom for label to hang
 
-			    		// if the ball is over the boundary divide its movement by 100 so it doesn't disappear out of viewport
-			    		c.vx += Math.cos( t ) * f / (hozBoundary ? 1 : 100);
-			    		c.vy += Math.sin( t ) * f / (verBoundary ? 1 : 100);
+						// if the ball is over the boundary divide its movement by 100 so it doesn't disappear out of viewport
+						c.vx += Math.cos( t ) * f / (hozBoundary ? 1 : 100);
+						c.vy += Math.sin( t ) * f / (verBoundary ? 1 : 100);
 
-			    	}
+						}
 
 				}
 				// end circle packing
@@ -205,20 +188,21 @@ app.balls = (function () {
 					// ADD ANCHOR AND TEXT
 					if (currentCircle.href) {
 						var ballAnchor = app.globals.doc.createElementNS('http://www.w3.org/2000/svg', 'a'),
-							path = app.globals.doc.createElementNS('http://www.w3.org/2000/svg', "path"),
-							textPath = app.globals.doc.createElementNS('http://www.w3.org/2000/svg', "textPath");
+							path = app.globals.doc.createElementNS('http://www.w3.org/2000/svg', 'path'),
+							textPath = app.globals.doc.createElementNS('http://www.w3.org/2000/svg', 'textPath');
 
-						path.setAttribute("id", "textPath" + i);
-						path.setAttribute("d", app.utilities.getPathData(currentCircle.r));
+						path.setAttribute('id', 'textPath' + i);
+						path.setAttribute('d', app.utilities.getPathData(currentCircle.r));
 
-						textPath.setAttribute("startOffset", "50%"); // cheeky bastard
-						textPath.setAttributeNS('http://www.w3.org/1999/xlink', "href", "#textPath" + i);
+						textPath.setAttribute('startOffset', '50%'); // cheeky bastard
+						textPath.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '#textPath' + i);
 						textPath.innerHTML = currentCircle.text;
 
 						groupEl.appendChild(path);
 
-						var text = app.globals.doc.createElementNS('http://www.w3.org/2000/svg', "text");
-							text.appendChild(textPath);
+						var text = app.globals.doc.createElementNS('http://www.w3.org/2000/svg', 'text');
+
+						text.appendChild(textPath);
 
 						ballAnchor.setAttribute('xlink:href', currentCircle.href);
 						ballAnchor.appendChild(polygonEl);
@@ -254,7 +238,7 @@ app.balls = (function () {
 					app.globals.svgEl.appendChild(groupEl);
 
 
-			    // UPDATE EXISTING BALL
+					// UPDATE EXISTING BALL
 				} else {
 					groupEl = appendedBalls[i];
 				}
@@ -262,41 +246,41 @@ app.balls = (function () {
 				currentCircle.vx *= spreadSpeed;
 				currentCircle.vy *= spreadSpeed;
 
-				var roundedY = Math.round((currentCircle.y+=currentCircle.vy) * 100) / 100,
-					roundedX = Math.round((currentCircle.x+=currentCircle.vx) * 100) / 100;
+				var roundedY = Math.round((currentCircle.y += currentCircle.vy) * 100) / 100,
+					roundedX = Math.round((currentCircle.x += currentCircle.vx) * 100) / 100;
 
 				//http://stackoverflow.com/a/28776528
 				if (app.utilities.isIE()) {
-					groupEl.setAttribute('transform', 'translate(' + roundedX + ', '+ roundedY + ')');
+					groupEl.setAttribute('transform', 'translate(' + roundedX + ', ' + roundedY + ')');
 
 				} else {
-					groupEl.style.webkitTransform = 'translate3d(' + roundedX + 'px, '+roundedY+'px, 0)';
+					groupEl.style.webkitTransform = 'translate3d(' + roundedX + 'px, ' + roundedY + 'px, 0)';
 					groupEl.style.transform = 'translate3d(' + roundedX + 'px, ' + roundedY + 'px, 0)';
 				}
 
-		    }
+			}
 
 
 
-		    app.interaction.updateInertia();
+			app.interaction.updateInertia();
 
-		    app.globals.animating = window.requestAnimationFrame(renderLoop);
+			app.globals.animating = window.requestAnimationFrame(renderLoop);
 
 		}
 
 
 		function startAnimationLoop() {
-		    if (!app.globals.animating) {
-		       renderLoop();
-		    }
+			if (!app.globals.animating) {
+				renderLoop();
+			}
 		}
 
 
 		function stopAnimationLoop() {
-		    if (app.globals.animating) {
-		       window.cancelAnimationFrame(app.globals.animating);
-		       app.globals.animating = undefined;
-		    }
+			if (app.globals.animating) {
+				window.cancelAnimationFrame(app.globals.animating);
+				app.globals.animating = undefined;
+			}
 		}
 
 
@@ -307,11 +291,6 @@ app.balls = (function () {
 			//stopAnimationLoop();
 
 		}, 1000);
-
-		// punch in the title card
-		/*setTimeout(function() {
-			app.globals.circleArr.push( createCircle('title') );
-		}, 1200);*/
 
 
 	};

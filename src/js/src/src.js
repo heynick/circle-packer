afee221ecfefe2f3bb17c4c5bd97e3ea9efd048c
Nick McMillan <nick.a.mcmillan@gmail.com>
2016-05-29 22:52:30 +1000

    updated gitignore and eslintrc

--- src/js/src/src.js
+++ src/js/src/src.js
@@ -2,15 +2,15 @@ var globals = require('./globals')
 
 
 window.onload = function() {
-    'use strict';
+    'use strict'
 
-    globals.doc.documentElement.classList.remove('no-js');
+    globals.doc.documentElement.classList.remove('no-js')
 
-    
+
 
 
     // initiate FastClick
     //FastClick.attach(document.body);
 
 
-};
+}
